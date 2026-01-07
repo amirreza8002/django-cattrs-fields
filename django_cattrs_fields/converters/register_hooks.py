2af21929eb06d499d1c385022596cee06705c741
amirreza sohrabi <amir.rsf1380@gmail.com>
2026-01-07 13:42:31 +0330

    add decimal field

--- django_cattrs_fields/converters/register_hooks.py
+++ django_cattrs_fields/converters/register_hooks.py
@@ -1,6 +1,7 @@
-from typing import Union
+from typing import Union, get_args
 
 from cattrs.converters import Converter
+from cattrs._compat import is_annotated
 
 from django.conf import settings
 
@@ -10,6 +11,7 @@ from django_cattrs_fields.fields import (
     DateField,
     DateTimeField,
     EmailField,
+    DecimalField,
     FloatField,
     IntegerField,
     SlugField,
@@ -30,6 +32,9 @@ from django_cattrs_fields.hooks import (
     datetime_structure,
     datetime_structure_nullable,
     datetime_unstructure,
+    decimal_structure,
+    decimal_structure_nullable,
+    decimal_unstructure,
     email_structure,
     email_structure_nullable,
     email_unstructure,
@@ -59,6 +64,9 @@ def register_structure_hooks(converter: Converter):
     converter.register_structure_hook(CharField, char_structure)
     converter.register_structure_hook(DateField, date_structure)
     converter.register_structure_hook(DateTimeField, datetime_structure)
+    converter.register_structure_hook_func(
+        lambda t: is_annotated(t) and get_args(t)[0] is DecimalField, decimal_structure
+    )
     converter.register_structure_hook(EmailField, email_structure)
     converter.register_structure_hook(FloatField, float_structure)
     converter.register_structure_hook(IntegerField, integer_structure)
@@ -70,6 +78,7 @@ def register_structure_hooks(converter: Converter):
     converter.register_structure_hook(Union[BooleanField, None], boolean_structure_nullable)
     converter.register_structure_hook(Union[CharField, None], char_structure_nullable)
     converter.register_structure_hook(Union[DateField, None], date_structure_nullable)
+    converter.register_structure_hook(Union[DecimalField, None], decimal_structure_nullable)
     converter.register_structure_hook(Union[DateTimeField, None], datetime_structure_nullable)
     converter.register_structure_hook(Union[EmailField, None], email_structure_nullable)
     converter.register_structure_hook(Union[FloatField, None], float_structure_nullable)
@@ -90,6 +99,7 @@ def register_unstructure_hooks(converter: Converter):
     converter.register_unstructure_hook(CharField, char_unstructure)
     converter.register_unstructure_hook(DateField, date_unstructure)
     converter.register_unstructure_hook(DateTimeField, datetime_unstructure)
+    converter.register_unstructure_hook(DecimalField, decimal_unstructure)
     converter.register_unstructure_hook(EmailField, email_unstructure)
     converter.register_unstructure_hook(FloatField, float_unstructure)
     converter.register_unstructure_hook(IntegerField, integer_unstructure)
@@ -102,6 +112,7 @@ def register_unstructure_hooks(converter: Converter):
     converter.register_unstructure_hook(Union[CharField, None], char_unstructure)
     converter.register_unstructure_hook(Union[DateField, None], date_unstructure)
     converter.register_unstructure_hook(Union[DateTimeField, None], datetime_unstructure)
+    converter.register_unstructure_hook(Union[DecimalField, None], decimal_unstructure)
     converter.register_unstructure_hook(Union[EmailField, None], email_unstructure)
     converter.register_unstructure_hook(Union[FloatField, None], float_unstructure)
     converter.register_unstructure_hook(Union[IntegerField, None], integer_unstructure)
--- django_cattrs_fields/fields/__init__.py
+++ django_cattrs_fields/fields/__init__.py
@@ -1,5 +1,16 @@
 import datetime
+from decimal import Decimal
 import uuid
+
+from attrs import frozen
+
+
+@frozen
+class Params:
+    """param class for passing extra data to fields"""
+
+    max_digits: int  # DecimalField
+    decimal_places: int  # DecimalField
 
 
 type BooleanField = bool
@@ -15,6 +26,8 @@ type URLField = str
 type UUIDField = uuid.UUID
 
 type IntegerField = int
+
+type DecimalField = Decimal
 
 type FloatField = float
 
